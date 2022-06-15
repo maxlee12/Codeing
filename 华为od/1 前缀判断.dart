891a77a9a453283b6e73d456b1cf857a46592724
MaxLi <760822901@qq.com>
2022-06-16 00:30:26 +0800

    202206160

--- 华为od/1 前缀判断.dart
+++ 华为od/1 前缀判断.dart
@@ -1,4 +1,3 @@
-
 // 1.输入一些单词 再输入一个前缀 输出所有前缀开始的单词 不包括相同的单词 按字典序排序 100分
 //
 // input:
@@ -8,24 +7,40 @@
 // output:
 // as asd asD
 
-void main(){
-  print(allPrefixString(['asd as as',"asD"],"a"));
+void main() {
+  print(allPrefixString(['asd as as', "asD", "ant's"], "a"));
+  print(allRegExpString(['asd as as', "asD", "ant's"], "a"));
 }
 
-
-List<String> allPrefixString(List<String> words,String prefix){
+List<String> allPrefixString(List<String> words, String prefix) {
   List<String> result = [];
-  for(String str in words){
-    String split = '\\pP|\\pS|\\pZ|\\pM';
+  for (String str in words) {
+    RegExp split = RegExp(r"[^a-zA-z]");
     List subString = str.split(split);
-    for(String subStr in subString){
-      if(subStr.startsWith(prefix)){
-        if(!result.contains(subStr)){
+    for (String subStr in subString) {
+      if (subStr.startsWith(prefix)) {
+        if (!result.contains(subStr)) {
           result.add(subStr);
         }
       }
     }
+  }
+  return result;
+}
 
+List<String> allRegExpString(List<String> words, String prefix) {
+  List<String> result = [];
+  for (String str in words) {
+    // RegExp exp = RegExp(r"\w+"); // 匹配字母、数字、下划线。等价于’[A-Za-z0-9_]’。
+    RegExp exp = RegExp(r"\W+"); // 匹配非字母、数字、下划线。等价于 ‘[^A-Za-z0-9_]’
+    List subString = str.split(exp);
+    for (String subStr in subString) {
+      if (subStr.startsWith(prefix)) {
+        if (!result.contains(subStr)) {
+          result.add(subStr);
+        }
+      }
+    }
   }
   return result;
 }
